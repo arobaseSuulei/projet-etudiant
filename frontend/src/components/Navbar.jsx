5961f84847c72821a1c804ee16913717b3d43ba4
suleyjs <souleymanediallo2004@outlook.fr>
2026-03-28 19:11:01 +0100

    branch front

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div className="fixed left-0 top-0 h-screen w-64 flex flex-col gap-4 p-6 border-r border-gray-200">
+      <p>Logo</p>
+      <p>Accueil</p>
+      <p>Profil</p>
+      <p>Messages</p>
+    </div>
+  )
+}
