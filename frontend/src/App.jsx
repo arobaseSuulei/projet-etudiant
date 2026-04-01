0afc2fccd5630bd6471bb87f757c94ce83bf4e79
suleyjs <souleymanediallo2004@outlook.fr>
2026-04-01 02:50:59 +0200

    all bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import Home from "./Home.jsx";
 import Messages from "./Messages.jsx";
 import NavbarPhone from "./Navbar-phone.jsx";
+import SearchBar from "./components/Search-bar.jsx";
 
 export default function App() {
     return (
         <Router>
             <Navbar />
             <NavbarPhone/>
+            <SearchBar/>
             <main className="sm:ml-64 bg-[#1c1c1e] min-h-screen">
                 {/* ml-64 = compense la largeur de la navbar */}
                 <Routes>
--- frontend/src/Navbar-phone.jsx
+++ frontend/src/Navbar-phone.jsx
@@ -44,19 +44,12 @@ export default function NavbarPhone() {
 
             </nav>
 
-            <nav className={'flex items-center gap-3 font-semibold text-lg px-4 py-2 rounded-full cursor-pointer  '}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                     stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round"
-                          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
-                </svg>
-
-            </nav>
 
             <nav className={'flex items-center gap-3 font-semibold text-lg px-4 py-2 rounded-full cursor-pointer  '}>
                 <div className="relative">
                     <Link to={'/messages'}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                             stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"/>
                         </svg>
                     </Link>
@@ -64,6 +57,18 @@ export default function NavbarPhone() {
                 </div>
 
             </nav>
+
+            <nav className={'flex items-center gap-3 font-semibold text-lg px-4 py-2 rounded-full cursor-pointer  '}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" className="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"/>
+                </svg>
+
+
+            </nav>
+
+
 
 
 
